18366c2e539a372a8ed13ecd065051d63b9d1cd3
d.ichko <d.ichko@corp.mail.ru>
2021-03-29 18:33:24 +0300

    Unify service result extension

--- backend/BrimSchedule.API/Utility/ControllerExtensions.cs
+++ backend/BrimSchedule.API/Utility/ControllerExtensions.cs
@@ -6,24 +6,29 @@ namespace BrimSchedule.API.Utility
 {
 	public static class ControllerExtensions
 	{
-		public static ActionResult Service<TModel>(this ControllerBase controller, ServiceResult<TModel> serviceResult)
+		public static IActionResult Service<TModel>(this ControllerBase controller, ServiceResult<TModel> serviceResult)
 		{
 			if (serviceResult == null)
 				throw new ArgumentNullException(nameof(serviceResult), "Service result can't be null");
 
-			return serviceResult.Success
-				? controller.Ok(serviceResult.Content)
-				: controller.BadRequest(serviceResult.ErrorMessage);
+			return HandleServiceResult(controller, serviceResult.Success, serviceResult.Content,
+				serviceResult.ErrorMessage);
 		}
 
-		public static ActionResult Service(this ControllerBase controller, ServiceResult serviceResult)
+		public static IActionResult Service(this ControllerBase controller, ServiceResult serviceResult)
 		{
 			if (serviceResult == null)
 				throw new ArgumentNullException(nameof(serviceResult), "Service result can't be null");
 
-			return serviceResult.Success
-				? controller.Ok()
-				: controller.BadRequest(serviceResult.ErrorMessage);
+			return HandleServiceResult(controller, serviceResult.Success, errorMsg: serviceResult.ErrorMessage);
 		}
+
+		private static IActionResult HandleServiceResult(ControllerBase controller, bool success, object content = null,
+			string errorMsg = null)
+			=> success
+				? content == null
+					? controller.Ok()
+					: controller.Ok(content)
+				: controller.BadRequest(errorMsg);
 	}
 }
